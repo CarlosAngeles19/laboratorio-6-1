e54813e841a4ef8e72a40004d235d2fbea831717
CarlosAngeles19 <carlosangeles.asc@gmail.com>
2023-09-20 21:03:26 -0500

    Agregar archivos de proyecto.

---
+++ laboratorio 6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class program
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("ingrese el numero 1:");
+       string n1txt=Console.ReadLine();
+       int num1 = int.Parse(n1txt);
+        
+        Console.Write("ingrese el numero 2 :");
+        string n2txt=Console.ReadLine();
+        int num2 = int.Parse(n2txt);
+
+
+        int resultado = 0;
+
+
+        for (int i = 0; i < num1; i++)
+        {
+            resultado = resultado + num2;
+        }
+        Console.WriteLine("el resultado es :" + resultado);
+        Console.ReadKey();
+    }
+}
+
